f2af7209f6ad26fcf593db06cdf07f0e2738d5b2
Zabis13 <lbsbmsu@mail.ru>
2026-02-25 19:52:48 +0300

    Add R package structure and dependencies

--- R/get_reachable_states_light.R
+++ R/get_reachable_states_light.R
@@ -12,39 +12,29 @@
 #'   \item{unique_states_count}{Number of unique states in the cycle}
 #' @export
 #' @examples
-#' # Quick cycle length check
-#' result <- get_reachable_states_light(1:20, c("L", "X", "L"), k = 4)
+#' result <- get_reachable_states_light(1:10, c("1", "3"), k = 4)
 #' cat("Cycle length:", result$total_moves, "\n")
 #' cat("Unique states:", result$unique_states_count, "\n")
-#'
-#' # Compare multiple sequences
-#' seq1 <- get_reachable_states_light(1:20, c("1", "3"), k = 4)
-#' seq2 <- get_reachable_states_light(1:20, c("2", "3"), k = 4)
-#' cat("Sequence 1 cycle:", seq1$total_moves, "\n")
-#' cat("Sequence 2 cycle:", seq2$total_moves, "\n")
 get_reachable_states_light <- function(start_state, allowed_positions, k) {
-  current_state <- start_state
-  visited <- new.env(hash = TRUE)
+  current_state <- as.integer(start_state)
+  visited_keys <- paste(current_state, collapse = "_")
   unique_states_count <- 1
   total_moves <- 0
 
-  state_hash <- digest::digest(current_state, algo = "md5")
-  visited[[state_hash]] <- TRUE
-
   repeat {
     for (op in allowed_positions) {
-      # Apply one operation
-      current_state <- apply_operations(current_state, op, k)
+      result <- apply_operations(current_state, op, k, NULL)
+      current_state <- result$state
 
       total_moves <- total_moves + 1
 
-      state_hash <- digest::digest(current_state, algo = "md5")
-      if (!exists(state_hash, envir = visited)) {
-        visited[[state_hash]] <- TRUE
+      state_key <- paste(current_state, collapse = "_")
+      if (!state_key %in% visited_keys) {
+        visited_keys <- c(visited_keys, state_key)
         unique_states_count <- unique_states_count + 1
       }
 
-      if (identical(current_state, start_state) && total_moves > 0) {
+      if (identical(current_state, as.integer(start_state)) && total_moves > 0) {
         return(list(
           total_moves = total_moves,
           unique_states_count = unique_states_count
